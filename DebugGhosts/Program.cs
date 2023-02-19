4f58f7248e1f549bfb1a5122447a13c2d6cdfe9e
Darren Tuit <dtui232@gmail.com>
2023-02-20 00:08:58 +1300

    debug code

--- DebugGhosts/Program.cs
+++ DebugGhosts/Program.cs
@@ -1,9 +1,24 @@
 ﻿using GBX.NET;
 using GBX.NET.Engines.Game;
+using System.Text;
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string Vec3ToString(Vec3 vec) {
+    return $"{vec.X}-{vec.Y}-{vec.Z}";
+}
+string QuatToString(Quat quat)
+{
+    return $"{quat.X}-{quat.Y}-{quat.Z}-{quat.W}";
+}
+string ByteArrayToString(byte[] bytes)
+{
+    var sb = new StringBuilder("");
+    foreach (var b in bytes)
+    {
+        sb.Append(b + ", ");
+    }
+    return sb.ToString();
+}
 
 
 //var filepath = "C:/Users/darren/Documents/Trackmania/Replays/AutosavedGhosts/Winter 2023 - 03/2023-01-02 06-01-56-Winter 2023 - 03-bolideblazar-25508ms.Replay.gbx";
@@ -32,13 +47,66 @@ Console.WriteLine("Hello, World!");
 
 //var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTestGroundContact3.Replay.Gbx";
 
-var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_bolideblazar_9-01-2023_3-14-37-am(00'13''976).Replay.Gbx";
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_bolideblazar_9-01-2023_3-14-37-am(00'13''976).Replay.Gbx";
 
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTestActionBlocks_DirtIceWaterRoadBetweenWithSlip.Replay.Gbx";
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_DirtIceGrassPlasticWaterRoadBetween_WithSlip.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTestGroundContact3.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_DirtIceGrassPlasticWaterRoadBetween_WhichWheelIsWhich.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_SlipUsingBreakOnlyRoad.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_SlipGrassAndRow_SingleTireSlip.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_Rotation.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_RotationFrontNoGround.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTestAirBrakeDirt.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_ReactorUpDownLv1Lv2.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_ReactorUpDownLv1Lv2.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_TurboTurbo2Turbo3ReactorUpReactorDownReactor2UpReactor2Down.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_CruiseControlNoBrakesNoEngineNoSteeringSlowMoFragile.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_ReactorUpAndDownTest.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_ReactorUpAndDownTest_2.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_ReactorUpLvl2AirControls.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_ReactorUpLvl2AirControls_AllCombos.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_WheelRotation_StartStop.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_WheelRotation_BackForward.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_SideSpeed_AND_TopContact.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_BoostAndSpin.Replay.Gbx";
+
+//var filepath = "C:\\Users\\darren\\Documents\\Trackmania\\Replays\\My Replays\\WheelStateTest_CameraChangeQ.Gbx";
+
+var filepath = "C:\\Users\\darren\\Documents\\TmForever\\Tracks\\Replays\\Autosaves\\darren_A05-Race.Replay.gbx";
 
 var replay = GameBox.Parse<CGameCtnReplayRecord>(filepath);
-
 //var samples = replay.Node.Ghosts[0].SampleData.Samples;
-
 var samples = replay.Node.Ghosts[0].RecordData.Samples;
+
+string fileName = Path.GetFileNameWithoutExtension(filepath);
+string outfilePath = $"C:\\Users\\darren\\code\\gbx-net-ghostdebug-data\\{fileName}_2.csv";
+
+using StreamWriter file = new(outfilePath);
+
+foreach (Sample sample in samples) {
+    //Console.WriteLine($"5:1 RPM {rpmByte} 14:1 Steer {steerByte}-{steer} 91:1 Gear {gearByte}-{gear} 15:1 u15 {u15} 18:1 brake/gas {brakeByte}-{brake}/{gas} 47:22 prsv {position}-{rotation}-{speed}-{velocity}");
+    var parsedVals = $"{sample.BufferType}, {Vec3ToString(sample.Position)}, {QuatToString(sample.Rotation)}, {Vec3ToString(sample.PitchYawRoll)}, {sample.Speed}, {Vec3ToString(sample.Velocity)}, {sample.Gear}, {sample.RPM}, {sample.Steer}, {sample.Brake}, {sample.Gas}, {sample.FLIcing}, {sample.RLIcing}, {sample.RRIcing}, {sample.FLDirt}, {sample.FRDirt}, {sample.RLDirt}, {sample.RRDirt}, {sample.FLGroundContactMaterial}, {sample.FRGroundContactMaterial}, {sample.RLGroundContactMaterial}, {sample.RRGroundContactMaterial}, {sample.FLDampenLen}, {sample.FRDampenLen}, {sample.RLDampenLen}, {sample.RRDampenLen}, {sample.FLSlipCoef}, {sample.FRSlipCoef}, {sample.RLSlipCoef}, {sample.RRSlipCoef}, {sample.WetnessValue}, {sample.IsGroundContact}, {sample.IsReactorGroundMode}, {sample.SideSpeed}";
+    file.WriteLine($"{sample.Timestamp},{sample.Data.Length}, {ByteArrayToString(sample.Data)},{parsedVals}");
+}
 
 Console.WriteLine("testing");
